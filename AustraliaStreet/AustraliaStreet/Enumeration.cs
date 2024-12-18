577ce358b3c8318b3a5094cd59c38d22e48cd8b2
Rosenberg Xiangyu Liu <xiangyu.liu@myprosperity.com.au>
2024-12-18 16:57:24 +1100

    Create Enumeration

---
+++ AustraliaStreet/AustraliaStreet/Enumeration.cs
@@ -0,0 +1,16 @@
+﻿namespace AustraliaStreet
+{
+    public abstract class Enumeration
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+
+        protected Enumeration(int id, string name, string abbreviation)
+        {
+            Id = id;
+            Name = name;
+            Abbreviation = abbreviation;
+        }
+    }
+}
